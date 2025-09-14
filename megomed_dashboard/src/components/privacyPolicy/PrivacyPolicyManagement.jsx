972aad8109651a2fbc07c9103fa7afce87de4081
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-09-14 18:15:47 +0600

    privacy policy

--- megomed_dashboard/src/components/privacyPolicy/PrivacyPolicyManagement.jsx
+++ megomed_dashboard/src/components/privacyPolicy/PrivacyPolicyManagement.jsx
@@ -1,39 +1,40 @@
-import { Button } from 'antd';
-import { useState } from 'react';
-import ClientPrivacyPolicy from './ClientPrivacyPolicy';
-import FreeLancherPrivacyPolicy from './FreeLancerPrivacyPolicy';
+import { Button } from "antd";
+import { useState } from "react";
+import ClientPrivacyPolicy from "./ClientPrivacyPolicy";
+import FreeLancherPrivacyPolicy from "./FreeLancerPrivacyPolicy";
 
 function PrivacyPolicyManagement() {
-  const [activeTab, setActiveTab] = useState('freelancher');
+  const [activeTab, setActiveTab] = useState("freelancher");
   return (
     <div className="p-6 bg-gray-50">
       <div className="mb-6 flex justify-between">
         <div>
           <Button
-            type={activeTab === 'freelancher' ? 'primary' : 'default'}
-            className={`mr-2 ${activeTab === 'freelancher' ? 'bg-[#002282]' : ''}`}
-            onClick={() => setActiveTab('freelancher')}
+            type={activeTab === "freelancher" ? "primary" : "default"}
+            className={`mr-2 ${
+              activeTab === "freelancher" ? "bg-[#002282]" : ""
+            }`}
+            onClick={() => setActiveTab("freelancher")}
           >
-            Freelancer
+            Privacy Policy
           </Button>
-          <Button
+          {/* <Button
             type={activeTab === 'client' ? 'primary' : 'default'}
             className={activeTab === 'client' ? 'bg-[#002282]' : ''}
             onClick={() => setActiveTab('client')}
           >
             client
-          </Button>
+          </Button> */}
         </div>
       </div>
 
       <div className=" rounded-md">
-        {activeTab === 'freelancher' ? (
+        {activeTab === "freelancher" ? (
           <FreeLancherPrivacyPolicy />
-        ) : (
-          <ClientPrivacyPolicy />
-        )}
+        ) : null
+        // <ClientPrivacyPolicy />
+        }
       </div>
-
     </div>
   );
 }
